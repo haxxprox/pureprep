fe7b1a6c88b7e382eed608f7ee3d4f4bdd14e2b1
haxxprox <geestyla87@gmail.com>
2026-05-19 20:45:12 +0200

    Add files via upload

--- netlify/functions/products.js
+++ netlify/functions/products.js
@@ -8,6 +8,7 @@ export const handler = async (event, context) => {
       { id: 2, name: 'Mechanische Tastatur', price: 89.50, image: 'https://images.unsplash.com/photo-1587829741301-dc798b83add3?w=400&q=80' },
       { id: 3, name: 'USB-C Hub', price: 45.00, image: 'https://images.unsplash.com/photo-1625842268584-8f3296236761?w=400&q=80' },
       { id: 4, name: 'Monitor 27"', price: 299.00, image: 'https://images.unsplash.com/photo-1527443224154-c4a3942d3acf?w=400&q=80' }
+      { id: 5, name: 'PurePreP"', price: 299.00, image: 'https://images.unsplash.com/photo-1625842268584-8f3296236761?w=400&q=80' }
     ];
 
     return {
